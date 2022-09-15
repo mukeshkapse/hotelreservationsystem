1457b040ae60dba0170196ab8e69a0977e9eefc4
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-15 20:30:21 +0530

    Added: welcome massage of hotel reservation system

---
+++ src/com/bridgelabz/hotelresarvationsystem/HotalReservation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.hotelresarvationsystem;
+
+public class HotalReservation {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To hotel reservation system");
+    }
+}
